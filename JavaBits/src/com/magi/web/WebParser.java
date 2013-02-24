4651b48636b91a5403d4bf4a058608bd05708c45
patkins <patkins@iinet.net.au>
2013-02-24 22:57:31 +1000

    initial commit

---
+++ JavaBits/src/com/magi/web/WebParser.java
@@ -0,0 +1,49 @@
+package com.magi.web;
+
+import java.io.InputStream;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+public class WebParser {
+
+	private String url;
+	
+	public WebParser(String url) {
+		this.url = url;
+	}
+	
+	public void parse() {
+		System.out.println("URL: " + url);
+		
+		try {
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpGet httpget = new HttpGet(url);
+			HttpResponse response = httpclient.execute(httpget);
+			HttpEntity entity = response.getEntity();
+			if (entity != null) {
+				int bread = 0;
+				byte[] buff = new byte[512];
+				StringBuffer sbuff = new StringBuffer();
+			    InputStream instream = entity.getContent();
+			    while ((bread = instream.read(buff)) != -1) {
+			    	sbuff.append(new String(buff,0,bread));
+			    }
+			    // Do not need the rest
+			    httpget.abort();
+			    
+			    parseText(sbuff.toString());
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected void parseText(String text) {
+		// Override this
+	}
+}
