b7a9a12305d51f1f45e2aa9045275d4d57adde07
patkins <patkins@iinet.net.au>
2013-02-27 23:11:59 +1000

    make class production ready (now used in a real application). Be clear about license.

--- JavaBits/src/main/java/com/magi/web/WebParser.java
+++ JavaBits/src/main/java/com/magi/web/WebParser.java
@@ -1,5 +1,8 @@
+/* Open Source Licensed under GNU LGPL 3.0
+ * See http://www.gnu.org/copyleft/lesser.html for details. */
 package com.magi.web;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 import org.apache.http.HttpEntity;
@@ -9,7 +12,9 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 /**
- * WebParser - Relies on HTTPClient 4.2
+ * WebParser - Relies on Apache HTTPClient 4.2.3
+ * Retrieves the HTML text content of a web page (URL).
+ * This class can be extended, or just instantiated and used.
  * 
  * @author patkins
  */
@@ -17,38 +22,46 @@ public class WebParser {
 
 	private String url;
 	
+	public WebParser() { }
+	
 	public WebParser(String url) {
 		this.url = url;
 	}
 	
-	public void parse() {
+	public String parse() throws IOException {
 		System.out.println("URL: " + url);
 		
-		try {
-			HttpClient httpclient = new DefaultHttpClient();
-			HttpGet httpget = new HttpGet(url);
-			HttpResponse response = httpclient.execute(httpget);
-			HttpEntity entity = response.getEntity();
-			if (entity != null) {
-				int bread = 0;
-				byte[] buff = new byte[512];
-				StringBuffer sbuff = new StringBuffer();
-			    InputStream instream = entity.getContent();
-			    while ((bread = instream.read(buff)) != -1) {
-			    	sbuff.append(new String(buff,0,bread));
-			    }
-			    // Do not need the rest
-			    httpget.abort();
-			    
-			    parseText(sbuff.toString());
-			}
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpGet httpget = new HttpGet(url);
+		HttpResponse response = httpclient.execute(httpget);
+		HttpEntity entity = response.getEntity();
+		if (entity != null) {
+			int bread = 0;
+			byte[] buff = new byte[512];
+			StringBuffer sbuff = new StringBuffer();
+		    InputStream instream = entity.getContent();
+		    while ((bread = instream.read(buff)) != -1) {
+		    	sbuff.append(new String(buff,0,bread));
+		    }
+		    // Do not need the rest
+		    httpget.abort();
+		    
+		    parseText(sbuff.toString());
+		    return sbuff.toString();
 		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
+		
+		return null;
 	}
 
 	protected void parseText(String text) {
 		// Override this
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
 	}
 }
